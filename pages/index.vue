d8072a72d4ef099cf7615917d39c4345b77738e0
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2021-08-02 22:24:47 +0600

    Some file update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <Tutorial/>
+
+  <div>
+    <h1>{{ apiValue }}</h1>
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      apiValue: '',
+    }
+  },
+  mounted(){
+    this.backendData()
+  },
+  methods:{
+    backendData(){
+      this.$axios.get('/demo').then(res => {
+        this.apiValue = res.data
+      })
+    }
+  }
+}
 </script>
